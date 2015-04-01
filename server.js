ced3379824bff608177b1011f6f9e719dad71955
Sujen Timila <sujentimila@gmail.com>
2015-04-01 18:12:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.use(express.static('./public'));
 app.get('/', function(req, res) {
 	res.sendfile('./public/index.html');
 });
-
+app.get('/storytime',function(req,res){
+	res.send("<p> Hey"+req.query.name+"nice to meet you! I think like to " req.query.verb+"this has been an awesome story of "+req.query.adjective=", you're totally welcome</p>");
+ });
 app.listen(app.get('port'), function() {
 	console.log('Fuck it ship it.');
 });
